cdbd6d656121ae439557b8f652a6ff837255b49e
Brook Alemu <brookwworku3@gmail.com>
2023-11-14 22:45:26 -0900

    creat header component

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import SignIn from "./pages/SignIn"
 import SignOut from "./pages/SingOut"
 import About from "./pages/About"
 import Profile from "./pages/Profile"
+import Header from "./components/Header"
  
 export default function App() {
 
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path = "/" element = {<Home/>}/>
       <Route path = "/sign-in" element = {<SignIn/>}/>
