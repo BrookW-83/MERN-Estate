ba885ff135b484038a40f3c7310652cd4594c6a4
Brook Alemu <brookwworku3@gmail.com>
2023-12-20 10:11:18 -0900

    creat a middel ware

--- api/controllers/auth_controller.js
+++ api/controllers/auth_controller.js
@@ -1,7 +1,8 @@
 import User from '../models/user_model.js'
 import bycryptjs from 'bcryptjs'
 
-export const signup = async (req, res) => {
+
+export const signup = async (req, res, next) => {
     const {username, email, password } = req.body;
     const hasedPassword = bycryptjs.hashSync(password, 10);
     const newUser = new User({username, email, password: hasedPassword})
@@ -10,6 +11,6 @@ export const signup = async (req, res) => {
         await newUser.save()
         res.status(201).json('Usser created successfully!')
     } catch (error) {
-        res.status(500).json(error.message)
+        next(error)
     }
 }
